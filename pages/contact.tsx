c559a309c8f87007b23223f58bb8e2d7548f0e37
Ondrej Elias <info@eliasondrej.com>
2019-09-12 22:10:39 +0200

    play with localization namespaces

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,18 @@
 import Layout from '@components/layouts/layout';
 import { withTranslation, i18n } from '@utils/i18n';
 
-const ContactPage = (props) => <Layout>{console.log(props.currentLanguage, i18n.language)}</Layout>;
+const ContactPage = ({ t, ...props }) => (
+    <Layout>
+        <div>
+            <h1>{t('page-title')}</h1>
+        </div>
+        {console.log(props.currentLanguage, i18n.language)}
+    </Layout>
+);
 
 ContactPage.getInitialProps = async ({ req }) => ({
     currentLanguage: req ? req.language : i18n.language,
-    namespacesRequired: ['common']
+    namespacesRequired: ['contact']
 });
 
-export default withTranslation('common')(ContactPage);
+export default withTranslation('contact')(ContactPage);
